eb3d8cf5a93c5f2257fae8f34bd1721f07702f6d
jlgualpa <jgualpa@uoc.edu>
2020-06-04 09:16:27 -0500

    PEC lll Eliminar Espacios y Comentarios

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-  function irPaginaWebpack() {
+function irPaginaWebpack() {
     var pregunta = confirm("Desea ir al enlace externo?")
     if (pregunta){
       var win = window.open('https://webpack.js.org/', '_blank');
       win.focus();
     }
-  }
-
-  function irPaginaRollup() {
+}
+function irPaginaRollup() {
     var pregunta = confirm("Desea ir al enlace externo?")
     if (pregunta){
       var win = window.open('http://rollupjs.org/guide/en/', '_blank');
       win.focus();
     }
-  }
-
-  window.onload = function() {
+}
+window.onload = function() {
     document.getElementById("boton_w").onclick = irPaginaWebpack;
     document.getElementById("boton_r").onclick = irPaginaRollup;
-  }
-
-
-
+}
